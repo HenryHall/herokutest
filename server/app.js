8df1e454677736e7441dcb3ca632db8fb324d37b
HenryHall <HenryHall@Henrys-MacBook-Pro.local>
2016-06-08 10:48:33 -0500

    quick fix

--- server/app.js
+++ server/app.js
@@ -5,5 +5,7 @@ var path = require('path');
 var server = app.listen(process.env.PORT || 3000);
 
 app.get('/', function (req, res){
+  res.writeHead(200);
   res.write("Hello World, its me!");
+  res.end();
 });
