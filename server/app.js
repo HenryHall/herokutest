60f2ae4e674ded3553a52bedbf6e791801a1658d
HenryHall <HenryHall@Henrys-MacBook-Pro.local>
2016-06-08 11:00:08 -0500

    quick fix2

--- server/app.js
+++ server/app.js
@@ -2,7 +2,8 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-var server = app.listen(process.env.PORT || 3000);
+var server = app.listen(process.env.PORT || 3000, function(){
+  console.log( 'server listening on port 3000' )});
 
 app.get('/', function (req, res){
   res.writeHead(200);
